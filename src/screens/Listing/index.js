704c5de628ae5f9dc05c7705d6e48282da96377d
Ananth Shanmugam <anshanmugam@informatica.com>
2022-12-21 18:23:53 -0500

    Working version with authenticator for both android and web

--- src/screens/Listing/index.js
+++ src/screens/Listing/index.js
@@ -1,30 +1,17 @@
 import { View, Text, Button } from "react-native";
 import React from "react";
 
-import { Authenticator, useAuthenticator } from "@aws-amplify/ui-react-native";
-
-function SignOutButton() {
-  const { signOut, authStatus, user } = useAuthenticator();
-
-  return (
-    <View style={{display:'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
-      <Text>User : {user.username} -- {user.attributes.email} -- {authStatus}</Text>
-      <Button title="Sign Out" onPress={signOut} />
-    </View>
-  );
-}
+import { withAuthenticator } from "aws-amplify-react-native";
 
 const Listing = () => {
   return (
-    <Authenticator.Provider>
-      <Authenticator>
-        <View>
-          <Text> All the listing goess here...</Text>
-        </View>
-        <SignOutButton />
-      </Authenticator>
-    </Authenticator.Provider>
+    <View>
+      <Text> All the listing goess here...</Text>
+    </View>
   );
 };
 
-export default Listing;
+export default withAuthenticator(Listing, {
+  // Render a sign out button once logged in
+  includeGreetings: true,
+});
